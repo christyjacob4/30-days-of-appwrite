b632857d1ec31cb567361b1a2f69b408d9c5a877
Damodar Lohani <dlohani48@gmail.com>
2021-04-27 16:27:43 +0545

    create post mde editor

---
+++ src/routes/Create.svelte
@@ -0,0 +1,21 @@
+<script>
+    import EasyMDE from "easymde";
+    import { onMount } from "svelte";
+    import "../../node_modules/easymde/dist/easymde.min.css";
+
+    onMount(() => {
+        let easyMDE = new EasyMDE();
+    });
+</script>
+
+<section>
+    <h2>Create Post</h2>
+    <form>
+        <textarea
+            name="content"
+            id="content"
+            cols="30"
+            rows="10"
+            placeholder="Enter content" />
+    </form>
+</section>
