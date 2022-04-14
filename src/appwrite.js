005846f0a0f5eeaf12d33fb52dbf0313e8dc9161
Bradley Schofield <ionicisere@gmail.com>
2022-04-14 11:02:02 +0100

    Update appwrite.js

--- src/appwrite.js
+++ src/appwrite.js
@@ -105,7 +105,7 @@ export const api = {
             limit,
             offset,
             undefined,
-            "after",
+            undefined,
             ["created_at"],
             ["DESC"]
         );
