5e4fd654455ff2665510b95f1304539417b82669
Matej Baco <matejbacocom@gmail.com>
2022-06-02 10:21:03 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -18,10 +18,9 @@ const config = {
 			precompress: false
 		}),
 		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
 			default: true
 		},
-		trailingSlash: 'always'
+		// trailingSlash: 'always'
 	},
 };
 
